54537b3ef24f6cce8df705a1beb19088110f1306
Nihar89bit <niharbunu23gmail.com>
2026-02-18 17:42:08 +0530

    Make database connection configurable via environment variables

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,15 +1,18 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "280404",
-  database: "emp"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "280404",
+  database: process.env.DB_NAME || "emp"
 });
 
 con.connect(function(err) {
   if (err) {
-    console.log("connection error");
+    console.log("connection error", err);
   } else {
     console.log("connected");
   }
